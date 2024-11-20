a6828654c07865a7e4bbbd431ab2e2f76bb70974
JofreCoca <jofrecoca@gmail.com>
2024-11-20 13:54:47 +0100

    todo implementado

--- src/Nivell_2_Exercici_1/Main.java
+++ src/Nivell_2_Exercici_1/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Smartphone smartphone=new Smartphone("Iphone","14");
+        smartphone.trucar("555345825");
+        smartphone.fotografiar();
+        smartphone.alarma();
     }
 }
--- src/Nivell_2_Exercici_1/Smartphone.java
+++ src/Nivell_2_Exercici_1/Smartphone.java
@@ -2,6 +2,9 @@ package Nivell_2_Exercici_1;
 
 public class Smartphone extends Telefon implements Camera,Rellotge{
 
+    public Smartphone(String marca,String model){
+        super(marca,model);
+    }
     public void fotografiar() {
         System.out.println("S'està fent una foto");
     }
